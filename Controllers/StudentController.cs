7fa1ffa91779d5d898303d9a56f4bd19257a1700
ranaabdullah32 <abdullahshamshad32@gmail.com>
2022-12-31 15:02:04 +0500

    Initial commit

---
+++ Controllers/StudentController.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+using crudoperations.Services;
+using crudoperations.Models;
+
+namespace crudoperations.Controllers
+{
+    public class StudentController : Controller
+    {
+        public IActionResult AddStudent()
+        {
+            return View(StudentService.GetAll());
+        }
+        [HttpPost]
+        public IActionResult AddStudent(StudentModel model)
+        {
+            StudentService.Add(model);
+            return View(StudentService.GetAll());
+        }
+        public IActionResult DeleteRecord(int id)
+        {
+            StudentService.Delete(id);
+            return RedirectToAction(nameof(AddStudent));
+        }
+        public IActionResult Update(int id)
+        {
+            return View(StudentService.GetbyId(id));
+        }
+        [HttpPost]
+        public IActionResult UpdateRecord(StudentModel tempmodel)
+        {
+            StudentService.Update(tempmodel);
+            return RedirectToAction(nameof(AddStudent));
+        }
+    }
+}
